e973b7bf5744f3697cc97c779d648e648dc66b5d
Fedor <qwertysdagger@gmail.com>
2017-06-21 17:14:36 +0300

    Update Program.cs: remove class Counter, move method in main

--- Program.cs
+++ Program.cs
@@ -2,29 +2,18 @@
 
 namespace task_DEV_1
 {
-    // Class that performs the function of a counter.
-    public class Counter
-    {
-        // Perform count from 0 to 100 and 
-        // print numbers to the console.
-        // Numbers that are multiples of 3 are output in 3 * N format.
-        public void Count()
-        {
-            string output;
-            for (int i = 0; i <= 100; i++)
-            {
-                output = (i % 3 != 0) ? i.ToString() : "3*" + (i / 3).ToString();
-                Console.WriteLine(output);
-            }
-        }
-    }
-
     static class Program
     {
         static void Main()
         {
-            Counter counter = new Counter();
-            counter.Count();
+            // Perform count from 0 to 100 and 
+            // print numbers to the console.
+            // Numbers that are multiples of 3 are output in 3 * N format.
+            for (int i = 0; i <= 100; i++)
+            {
+                string output = (i % 3 != 0) ? i.ToString() : "3*" + (i / 3).ToString();
+                Console.WriteLine(output);
+            }
         }
     }
 }
