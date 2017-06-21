a4048859e2a89411a25021c59cd6bfed873ff40a
Fedor <qwertysdagger@gmail.com>
2017-06-21 10:35:44 +0300

    Add Program.cs - for task_DEV-2

--- Program.cs
+++ Program.cs
@@ -1,20 +1,41 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace task_DEV_1
+namespace task_DEV_2
 {
     // Class that performs the function of a counter.
     public class Counter
     {
-        // Perform count from 0 to 100 and 
-        // print numbers to the console.
-        // Numbers that are multiples of 3 are output in 3 * N format.
+        // Perform count from 0 to 100 and print numbers to the console.
+        // Numbers that are multiples of 3 are replaced by "Tutti".
+        // Numbers that are multiples of 5 are replaced by "Frutti".
+        // Numbers that are multiples of 15 are replaced by "Tutti-Frutti".
         public void Count()
         {
-            string output;
             for (int i = 0; i <= 100; i++)
             {
-                output = (i % 3 != 0) ? i.ToString() : "3*" + (i / 3).ToString();
-                Console.WriteLine(output);
+                if (i % 15 != 0)
+                {
+                    if (i % 3 == 0)
+                    {
+                        Console.WriteLine("Tutti");
+                        continue;
+                    }
+                    if (i % 5 == 0)
+                    {
+                        Console.WriteLine("Frutti");
+                        continue;
+                    }
+
+                    Console.WriteLine(i);
+                }
+                else
+                {
+                    Console.WriteLine("Tutti-Frutti");
+                }
             }
         }
     }
