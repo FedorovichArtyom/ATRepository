1d9af6f34bbaadfa3b36b3e4fc98c2245e320a32
Fedor <qwertysdagger@gmail.com>
2017-06-24 14:22:00 +0300

    Modified program for numbers up to 254 symbols.

--- task_DEV-3/Program.cs
+++ task_DEV-3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace task_DEV_3
 {
@@ -9,22 +10,92 @@ namespace task_DEV_3
             Console.WriteLine("Enter a non-negative integer number.");
             string inputValue = Console.ReadLine();
 
-            //try catch
-            int inputNumber = Convert.ToInt32(inputValue);
+            //разбиение строки числа на элементы массива
+            int inputNumberPartSize = int.MaxValue.ToString().Length - 1;
+            int inputNumberPartsAmount = inputValue.Length / 
+                (inputNumberPartSize) + 1;
+            int[] inputNumberAsArray = new int[inputNumberPartsAmount];
 
-            int[] previousRowMembers = new int[2];
-            previousRowMembers[0] = 0;
-            previousRowMembers[1] = 1;
-            int currentRowMember = 1;
-
-            while (currentRowMember < inputNumber)
+            try
+            {  
+                for (int i = 0; i < inputNumberPartsAmount - 1; i++)
+                {
+                    inputNumberAsArray[i] = Convert.ToInt32(inputValue.Substring(
+                        (inputNumberPartsAmount - 2 - i) * (inputNumberPartSize) +
+                        inputValue.Length % (inputNumberPartSize),
+                        inputNumberPartSize));
+                }
+                inputNumberAsArray[inputNumberPartsAmount - 1] = Convert.ToInt32(inputValue.Substring
+                    (0, inputValue.Length % (inputNumberPartSize)));
+            }
+            catch (FormatException)
             {
-                currentRowMember = previousRowMembers[0] + previousRowMembers[1];
-                previousRowMembers[0] = previousRowMembers[1];
-                previousRowMembers[1] = currentRowMember;
+                Console.WriteLine("Sorry, the entered number is incorrect! Exiting process.");
+                Environment.Exit(-1);
+            }
+            
+            //Вычисление всякой херни
+            int[,] previousRowMembers = new int[2, inputNumberPartsAmount];
+            previousRowMembers[0, 0] = 0;
+            previousRowMembers[1, 0] = 1;
+            int[] currentRowMember = new int[inputNumberPartsAmount];
+            currentRowMember[0] = 1;
+
+            bool completeCalculation = false;
+            while (true)
+            {
+                //сравнение чисел
+                for (int i = inputNumberPartsAmount - 1; i >= 0; i--)
+                {
+                    if (inputNumberAsArray[i] != currentRowMember[i])
+                    {
+                        if (inputNumberAsArray[i] < currentRowMember[i])
+                        {
+                            completeCalculation = true;
+                            break;
+                        }
+                        if (inputNumberAsArray[i] > currentRowMember[i])
+                        {
+                            break;
+                        }
+                    }
+                }
+
+                if (completeCalculation)
+                {
+                    break;
+                }
+
+                //сложение чисел
+                int previousDigitNumber = 0;
+                for (int i = 0; i < inputNumberPartsAmount; i++)
+                {
+                    if ((previousRowMembers[0, i] + previousRowMembers[1, i] + previousDigitNumber).
+                       ToString().Length > inputNumberPartSize)
+                    {
+                        currentRowMember[i] = (previousRowMembers[0, i] + previousRowMembers[1, i] +
+                            previousDigitNumber) %
+                            Convert.ToInt32(Math.Pow(10, inputNumberPartSize));
+                        previousDigitNumber = 1;
+                    }
+                    else
+                    {
+                        currentRowMember[i] = previousRowMembers[0, i] + previousRowMembers[1, i] +
+                            previousDigitNumber;
+                        previousDigitNumber = 0;
+                    }
+
+                    previousRowMembers[0, i] = previousRowMembers[1, i];
+                    previousRowMembers[1, i] = currentRowMember[i];
+                }    
             }
 
-            string outputMessage = (currentRowMember == inputNumber) || (inputNumber == 0)
+            for (int i = 0; i < inputNumberPartsAmount; i++)
+            {
+                currentRowMember[i] = previousRowMembers[0, i];
+            }
+            //Вывод итогового сообщения
+            string outputMessage = currentRowMember.SequenceEqual(inputNumberAsArray)
                 ? "The entered number is a member of the Fibonacci sequence."
                 : "The entered number isn't a member of the Fibonacci sequence.";
 
