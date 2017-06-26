847c0fba6b8c5374264466b6cec7f3aaf9170603
Fedor <qwertysdagger@gmail.com>
2017-06-26 15:34:19 +0300

    Refactored code, added new classes

---
+++ task_DEV-4/IntegerNumberSequence.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Numerics;
+
+namespace task_DEV_4
+{
+    // Class describing the sequence of integers in the form of the BigInteger array.
+    public class IntegerNumberSequence
+    {
+        // Array specifying the sequence to be examined.
+        private BigInteger[] numberSequence;
+        // A sequence property that determines whether it is non-decreasing.
+
+        public IntegerNumberSequence(BigInteger[] sequence)
+        {
+            numberSequence = sequence;
+        }
+
+        // Set the value of the sequence.
+        public BigInteger[] SequenceValues
+        {
+            set
+            {
+                numberSequence = value;
+            }
+        }
+
+        // A method to determine whether a sequence is non-decreasing.
+        public bool CheckSequenceForNonDecreasing()
+        {
+            bool isNonDecreasing = true;
+            BigInteger previousMember = numberSequence[0];
+            foreach (BigInteger member in numberSequence)
+            {
+                if (member < previousMember)
+                {
+                    isNonDecreasing = false;
+                    break;
+                }
+                previousMember = member;
+            }
+            return isNonDecreasing;
+        }
+    }
+}
