1cbedc04ea890080204872e863254bf086f87c5e
Fedor <qwertysdagger@gmail.com>
2017-10-06 20:08:59 +0300

    Implemented TransliterationHelper, added input data and alphabet file.

---
+++ task_DEV-11/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace task_DEV_11
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string[] alphabetFromFile = null;
+      string dataFromFile;
+      string afterTransliterate;
+
+      var fromRusToLatinAlphabet = new Dictionary<string, string>();
+      var fromLatinToRusAlphabet = new Dictionary<string, string>();
+
+      try
+      {
+        // Get from rus to latin alphabet from file.
+        string filePath = args[0];
+        using (TextReader textReader = new StreamReader(args[0]))
+        {
+          char[] delimiters = { '[', ']', '\r', '\n' };
+          alphabetFromFile = textReader.ReadToEnd()
+            .Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        // Add it to the fromRusToLatinAlphabet dictionary.
+        foreach (var value in alphabetFromFile)
+        {
+          char[] delimiters = { ' ', '-' };
+          string[] formants = value.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+          fromRusToLatinAlphabet.Add(formants[0], formants[1]);
+        }
+
+        // Get from latin to rus alphabet from file.
+        filePath = args[1];
+        using (TextReader textReader = new StreamReader(args[1]))
+        {
+          char[] delimiters = { '[', ']', '\r', '\n' };
+          alphabetFromFile = textReader.ReadToEnd()
+            .Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        // Add it to the fromLatinToRusAlphabet dictionary.
+        foreach (var value in alphabetFromFile)
+        {
+          char[] delimiters = { ' ', '-' };
+          string[] formants = value.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+          fromRusToLatinAlphabet.Add(formants[0], formants[1]);
+        }
+
+        // Get input string from File
+        using (TextReader textReader = new StreamReader(args[1]))
+        {
+          dataFromFile = textReader.ReadToEnd();
+        }
+
+        // Do transliteration.
+        afterTransliterate = new TransliterationHelper().Transliterate(dataFromFile, fromRusToLatinAlphabet, 
+          fromLatinToRusAlphabet);
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine(ex.Message);
+        return;
+      }
+
+      // Output the result of transliteration.
+      Console.WriteLine(afterTransliterate);
+    }
+  }
+}
---
+++ task_DEV-11/TransliterationHelper.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace task_DEV_11
+{
+  public class TransliterationHelper
+  {
+    public string Transliterate(string input, Dictionary<string, string> fromRusToLatinAlphabet,
+      Dictionary<string, string> fromLatinToRusAlphabet)
+    {
+      // Check first symbol of the input string if it is latin or rus.
+      bool isInputLatin = true;
+      foreach (var value in fromRusToLatinAlphabet.Keys)
+      {
+        if (input[0].ToString() == value)
+        {
+          isInputLatin = false;
+          break;
+        }
+      }
+      // Now will think that the whole input string is latin, if the first symbol is latin.
+      // If it is not, it will get an exception further.
+      // Same for the rus string.
+      // Choose the way to transliterate and do transliteration.
+      return isInputLatin
+        ? TransliterateFromLatinToRus(input, fromRusToLatinAlphabet, fromLatinToRusAlphabet)
+        : TransliterateFromRusToLatin(input, fromRusToLatinAlphabet, fromLatinToRusAlphabet);
+    }
+
+    public string TransliterateFromRusToLatin(string input, Dictionary<string, string> fromRusToLatinAlphabet,
+      Dictionary<string, string> fromLatinToRusAlphabet)
+    {
+      string transliteratedInput = null;
+      foreach (var value in fromLatinToRusAlphabet.Keys)
+      {
+        if (input.Contains(value))
+        {
+          throw new Exception();
+        }
+      }
+
+      foreach (var value in fromRusToLatinAlphabet.Keys)
+      {
+        transliteratedInput = input.Replace(value, fromRusToLatinAlphabet[value]);
+      }
+      return transliteratedInput;
+    }
+
+    //
+    public string TransliterateFromLatinToRus(string input, Dictionary<string, string> fromRusToLatinAlphabet,
+      Dictionary<string, string> fromLatinToRusAlphabet)
+    {
+      string transliteratedInput = null;
+      foreach (var value in fromRusToLatinAlphabet.Keys)
+      {
+        if (input.Contains(value))
+        {
+          throw new Exception();
+        }
+      }
+
+      foreach (var value in fromLatinToRusAlphabet.Keys)
+      {
+        transliteratedInput = input.Replace(value, fromLatinToRusAlphabet[value]);
+      }
+      return transliteratedInput;
+    }
+  }
+}
