f94a02f5cc2bbfeb119723d257cee67372a61961
Fedor <qwertysdagger@gmail.com>
2017-11-07 13:43:22 +0200

    Working version.

--- task_DEV-13/Company/DevGroupProvider.cs
+++ task_DEV-13/Company/DevGroupProvider.cs
@@ -9,12 +9,12 @@ namespace task_DEV_13
     private int customerEfficiency;
 
     private List<DeveloperQualification> efficiencyHierarchy = new List<DeveloperQualification>
-      {
-        new DeveloperQualification(QualificationName.Junior),
-        new DeveloperQualification(QualificationName.Middle),
-        new DeveloperQualification(QualificationName.Senior),
-        new DeveloperQualification(QualificationName.Lead)
-      };
+    {
+      new DeveloperQualification(QualificationName.Junior),
+      new DeveloperQualification(QualificationName.Middle),
+      new DeveloperQualification(QualificationName.Senior),
+      new DeveloperQualification(QualificationName.Lead)
+    };
 
     public DevGroupProvider(decimal customerPrice, int customerEfficiency)
     {
@@ -22,20 +22,27 @@ namespace task_DEV_13
       this.customerEfficiency = customerEfficiency;
     }
 
+    // Provide group methods.
     public DevGroup GetDevGroupWithMaxEfficiencyPerPrice()
     {
       efficiencyHierarchy.Sort(this.CompareQualificationsByEfficiencyPerPrice);
 
       return BuildGroup();
     }
-
     public DevGroup GetDevGroupWithMinPricePerEfficiency()
     {
       efficiencyHierarchy.Sort(this.CompareQualificationsByPricePerEfficiency);
 
       return BuildGroup();
     }
+    public DevGroup GetDevGroupWithMinNonJuniorsPerEfficiency()
+    {
+      efficiencyHierarchy.Sort(this.CompareQualificationsByMinNonJuniorsPerEfficiency);
 
+      return BuildGroup();
+    }
+
+    // Build group helper.
     private DevGroup BuildGroup()
     {
       DevGroup group = new DevGroup();
@@ -59,6 +66,7 @@ namespace task_DEV_13
       return group;
     }
 
+    // Comparers.
     public int CompareQualificationsByEfficiencyPerPrice(DeveloperQualification firstQualification,
       DeveloperQualification secondQualification)
     {
@@ -115,6 +123,28 @@ namespace task_DEV_13
         {
           result = 0;
         }
+      }
+
+      return result;
+    }
+    public int CompareQualificationsByMinNonJuniorsPerEfficiency(DeveloperQualification firstQualification,
+      DeveloperQualification secondQualification)
+    {
+      int result = 0;
+      if (firstQualification.Qualification == QualificationName.Junior &&
+        secondQualification.Qualification != QualificationName.Junior)
+      {
+        result = -1;
+      }
+      if (firstQualification.Qualification != QualificationName.Junior &&
+        secondQualification.Qualification == QualificationName.Junior)
+      {
+        result = 1;
+      }
+      if (firstQualification.Qualification != QualificationName.Junior &&
+        secondQualification.Qualification != QualificationName.Junior)
+      {
+        result = CompareQualificationsByPricePerEfficiency(firstQualification, secondQualification);
       }
 
       return result;
